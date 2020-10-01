4e0716b760d0f0c34a12ddb9f28ec913f4c77c9e
Edgar Tapia <edgartapia@MacBook-Pro-de-Edgar.local>
2020-10-01 02:15:15 -0500

    [update] Se crea 2 nuevos servicios GET

--- app.ts
+++ app.ts
@@ -4,11 +4,56 @@ import {
   RouterContext,
 } from "https://deno.land/x/oak/mod.ts";
 
+enum Gender {
+  Male,
+  Female,
+}
+
+interface IUser {
+  username: string;
+  email: string;
+  age: number;
+  gender: Gender;
+  password: string
+}
+
+let Users: Array<IUser> = [
+  {
+    username: "edgar",
+    email: "edgar.tapia@outlook.es",
+    age: 33,
+    gender: Gender.Male,
+    password: "qwerty"
+  },
+  {
+    username: "ethien",
+    email: "ethien.salinas@gmail.com",
+    age: 35,
+    gender: Gender.Male,
+    password: "qwerty"
+  },
+  {
+    username: "alex",
+    email: "alex.gallegos@gmail.com",
+    age: 25,
+    gender: Gender.Male,
+    password: "qwerty"
+  },
+
+]
+
 const app = new Application();
 const userRouter = new Router();
 
 userRouter.get("/users", (ctx: RouterContext) => {
   ctx.response.body = "[GET] /users has been call";
+});
+
+const findUser = async(username:string | undefined) => Users.find(x => x.username == username);
+
+userRouter.get("/users/:username", async(ctx: RouterContext) => {
+  const { params } = ctx;
+  ctx.response.body = `[GET] ${JSON.stringify(await findUser(params.username))}`;
 });
 
 userRouter.post("/users", async (ctx: RouterContext) => {
@@ -19,8 +64,8 @@ userRouter.post("/users", async (ctx: RouterContext) => {
     msg: "[POST] /users",
     request: {
       type: reqBody.type,
-      value: await reqBody.value
-    }
+      value: await reqBody.value,
+    },
   };
 });
 userRouter.put("/users", (ctx: RouterContext) => {
